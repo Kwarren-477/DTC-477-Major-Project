96eb3a55ee77273ddd639fb18173a572a7ff6f08
Kwarren-477 <kayla.l.warren@wsu.edu>
2026-04-30 16:32:46 +0000

    Added some Java to buttons

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 
 //dad joke site functions//
 
+//dad joke api//
+
+const jokeurl = 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes';
+const jokeoptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'be6a03c956mshaee959d6d0ade22p1fb573jsna6b7c2d29696',
+		'x-rapidapi-host': 'dad-jokes-by-api-ninjas.p.rapidapi.com',
+		'Content-Type': 'application/json'
+	}
+};
+
 //urband dictoanry api//
 
 const slangurl = 'https://urban-dictionary7.p.rapidapi.com/v0/define?term=yeet';
@@ -13,22 +25,6 @@ const slangoptions = {
 	}
 };
 
-//dad joke api//
-
-const jokeurl = 'https://dad-jokes.p.rapidapi.com/joke/ai/cow';
-const jokeoptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'be6a03c956mshaee959d6d0ade22p1fb573jsna6b7c2d29696',
-		'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	}
-};
-
-let scrollingBox;
-let offsetLeftStart;
-let scrollLeftStart;
-let isMoving;
 
 
 
@@ -48,36 +44,46 @@ async function getData(url, options) {
 }
 
 
-//api look up//
-let ipLookupURL = "https://api.ipify.org/?format=json";
-let ipLookupOptions = {};
+// make function to get jokes
+function getJokes() {
 
+    getData(jokeurl, jokeoptions).then(function(result){
 
- ///Hardware for DOM loading//
-
- 
- {(
-        
-
-        scrollingBox.addEventListener("mouseleave", function (e) {
-            isMoving = false;
-        });
-
-        scrollingBox.addEventListener("mouseup", function (e) {
-            isMoving = false;
-        });
-
-        scrollingBox.addEventListener("mousemove", function (e) {
-            e.preventDefault();
-            if (!isMoving) return;
-            scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
-        });
-
-
+        console.log(result);
+        document.querySelector("#jokeText").innerHTML = result[0].joke;
 
     });
 
+}
 
 
+document.addEventListener("DOMContentLoaded", function(){
 
+    getJokes();
+
+  document.querySelector("#newJoke").addEventListener("click", getJokes(){
+
+});
+
+}
+
+/*save Joke function*/
+
+function saveJokes() {
+
+
+     console.log(result);
+        document.querySelector("#saveJokes").innerHTML = result[1].save;
+
+    });
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    saveJokes();
+
+  document.querySelector("#copyJoke").addEventListener("click", saveJokes(){
+
+});
+
+}
 
